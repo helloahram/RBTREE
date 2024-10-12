1c11e04319dce29e6029e20e400a750dd81edbd8
ahram2 <helloahram1@gmail.com>
2024-10-12 16:06:17 +0900

    RBTREE Find

--- src/rbtree.c
+++ src/rbtree.c
@@ -36,7 +36,29 @@ node_t *rbtree_insert(rbtree *t, const key_t key)
 node_t *rbtree_find(const rbtree *t, const key_t key)
 {
   // TODO: implement find
-  return t->root;
+  // root node 부터 탐색 시작
+  node_t *current = t->root;
+
+  // nil->node 도달까지 (Key 를 못 찾을 때까지) 탐색 진행
+  // 찾는 값 key 과 현재 값 current->key 를 비교
+  // Current 와 비교했을 때 Key 가 작다면 왼쪽으로 이동
+  // Current 와 비교했을 때 Key 가 크다면 오른쪽으로 이동
+  while (current->key != t->nil)
+  {
+    // 현재 값보다 찾는 값이 클 때 왼쪽으로 이동
+    if (current->key > key)
+      current = current->left;
+    // 현재 값보다 찾는 값이 작을 때 오른쪽으로 이동
+    else if (current->key < key)
+      current = current->right;
+    // Key 를 찾았을 때 해당 노드 current 반환
+    else
+      return current;
+  }
+
+  // nil node 까지 도달했지만 Key 가 없는 경우,
+  // node_t * 반환 타입에 맞게 NULL return
+  return NULL;
 }
 
 node_t *rbtree_min(const rbtree *t)
