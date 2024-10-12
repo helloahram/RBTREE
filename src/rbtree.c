daa3ed73b15019b00c93d47b55471c70a7d97be5
ahram2 <helloahram1@gmail.com>
2024-10-12 16:00:45 +0900

    RBTREE Find

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,43 +2,89 @@
 
 #include <stdlib.h>
 
-rbtree *new_rbtree(void) {
+rbtree *new_rbtree(void)
+{
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+
+  p->nil = (node_t *)calloc(1, sizeof(node_t));
+  if (NULL == p->nil)
+  {
+    free(p);
+    return NULL;
+  }
+  // calloc 으로 메모리 할당 + 초기화 진행
+
+  p->nil->color = RBTREE_BLACK;
+  p->root = p->nil;
+
   return p;
 }
 
-void delete_rbtree(rbtree *t) {
+void delete_rbtree(rbtree *t)
+{
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
   // TODO: implement insert
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_min(const rbtree *t) {
+node_t *rbtree_min(const rbtree *t)
+{
   // TODO: implement find
-  return t->root;
+  // nil node 를 제외하고 가장 왼쪽 자식에 있는 자식 노드가
+  // 트리에서 가장 작은 값이기 때문에 끝까지 탐색
+
+  // root node 부터 탐색 시작
+  node_t *current = t->root;
+
+  // current->left 가 nil node 인 경우 가장 작은 값이므로
+  // current->left 가 nil node 일 때까지 탐색
+  while (current->left != t->nil)
+  {
+    current = current->left;
+  }
+
+  return current;
 }
 
-node_t *rbtree_max(const rbtree *t) {
+node_t *rbtree_max(const rbtree *t)
+{
   // TODO: implement find
-  return t->root;
+  // nil node 를 제외하고 가장 오른쪽 자식에 있는 자식 노드가
+  // 트리에서 가장 큰 값이기 때문에 끝까지 탐색
+
+  // root node 부터 탐색 시작
+  node_t *current = t->root;
+
+  // current->left 가 nil node 인 경우 가장 작은 값이므로
+  // current->left 가 nil node 일 때까지 탐색
+  while (current->right != t->nil)
+  {
+    current = current->right;
+  }
+
+  return current;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p)
+{
   // TODO: implement erase
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+{
   // TODO: implement to_array
   return 0;
 }
--- src/rbtree.h
+++ src/rbtree.h
@@ -3,19 +3,31 @@
 
 #include <stddef.h>
 
-typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+// RBTREE 색을 열거형으로 정의하고, color_t 라는 이름으로 새로운 자료형 정의
+// Red, Black 를 각각 RBTREE_xxx 으로 정의하여 color_t nodeColor; 로 사용 가능
+// ex) nodeColor = RBTREE_RED; or nodeColor = RBTREE_BLACK;
+typedef enum
+{
+  RBTREE_RED,
+  RBTREE_BLACK
+} color_t;
 
+// RBTREE 에서 사용할 정수형 키 값을 key_t 라는 이름으로 정의
 typedef int key_t;
 
-typedef struct node_t {
-  color_t color;
-  key_t key;
-  struct node_t *parent, *left, *right;
+// RBTREE 의 노드를 나타내는 구조체 정의
+typedef struct node_t
+{
+  color_t color;                        // 노드의 색상 RED or BLACK
+  key_t key;                            // 노드가 저장하는 키 값
+  struct node_t *parent, *left, *right; // 부모, 왼쪽 자식, 오른쪽 자식을 나타내는 포인터
 } node_t;
 
-typedef struct {
-  node_t *root;
-  node_t *nil;  // for sentinel
+// RBTREE 자체를 나타내는 구조체 정의
+typedef struct
+{
+  node_t *root; // 트리의 루트 노드를 가리키는 포인터
+  node_t *nil;  // nil 노드를 가리키는 포인터, Sentinel 로 사용
 } rbtree;
 
 rbtree *new_rbtree(void);
@@ -29,4 +41,4 @@ int rbtree_erase(rbtree *, node_t *);
 
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
 
-#endif  // _RBTREE_H_
+#endif // _RBTREE_H_
